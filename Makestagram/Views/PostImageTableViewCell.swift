ecde3210397b2e434d0c480cb965d76ac0f3d01f
Asher <asher.rosenblatt@me.com>
2017-06-27 15:41:31 -0500

    added kingfisher to podfile and ran install

---
+++ Makestagram/Views/PostImageTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostImageTableViewCell.swift
+//  Makestagram
+//
+//  Created by Asher Rosenblatt on 6/27/17.
+//  Copyright © 2017 AVRTech. All rights reserved.
+//
+
+import UIKit
+
+class PostImageTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var postImageView: UIImageView!
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        
+    }
+
+}
--- Podfile
+++ Podfile
@@ -11,6 +11,7 @@ target 'Makestagram' do
   pod 'FirebaseUI/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'Kingfisher', '~> 3.0'
 
   target 'MakestagramTests' do
     inherit! :search_paths
